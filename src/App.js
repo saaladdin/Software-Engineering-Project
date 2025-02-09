003fb5eaa2adfc3ce321b1b996792e55a06ba2e2
Kevin Calle <kcalle012@gmail.com>
2025-02-09 10:15:27 -0500

    added login page and styling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import './App.scss';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import Header from "./Components/Header"
 import Footer from "./Components/Footer"
 import Landing from  "./Components/Landing"
 import Login from "./Components/Login"
 
+
 function App() {
+
+  const location = useLocation();
+  const hideHeaderRoutes = ["/login"];
+
   return (
     <div className="App">
-      <Header />
+      {/* 
+      Checks if we are not in login page, if we aren't we will
+      render the header 
+      */}
+      {!hideHeaderRoutes.includes(location.pathname) && <Header />}
       <Routes>
           <Route index element={<Landing />}/>
           <Route path="/login" element={<Login />}/>
       </Routes>
-      <Footer />
+      {!hideHeaderRoutes.includes(location.pathname) && <Footer />}
     </div>
   );
 }
--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -4,7 +4,18 @@ import  "./index.scss"
 const Login = () => {
     return (
         <div className="login">
-            Login
+            <form className="login-form" action="">
+                <div className="welcome-message">
+                    <p>Welcome to</p>
+                    <h1>Event Planner</h1>
+                </div>
+
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" name="email"/>
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" name="password"/>
+                <button className="submit" type="submit">Login</button>
+            </form>
         </div>
     )
 }
