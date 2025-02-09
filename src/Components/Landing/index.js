e074974f20dfa1da890fed039d10d5fcd52891af
Kevin Calle <kcalle012@gmail.com>
2025-02-09 09:16:16 -0500

    added browser router and components for first milestone

---
+++ src/Components/Landing/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.scss"
+
+const Landing = () => {
+    return (
+        <div className="landing">
+            Landing
+        </div>
+    )
+}
+
+export default Landing;
---
+++ src/Components/Login/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import  "./index.scss"
+
+const Login = () => {
+    return (
+        <div className="login">
+            Login
+        </div>
+    )
+}
+
+export default Login;
