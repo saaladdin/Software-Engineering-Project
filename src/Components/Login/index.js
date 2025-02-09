47116c9adfea8829d63a3bba242fd6d9d179d47a
Kevin Calle <kcalle012@gmail.com>
2025-02-09 11:39:04 -0500

    added shaowing and fixed spacing

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import  "./index.scss"
+import Image from "../../Assets/Images/login-image.png"
 
 const Login = () => {
     return (
         <div className="login">
+            <img src={Image} alt="Image of two people watering a plant" />
             <form className="login-form" action="">
                 <div className="welcome-message">
                     <p>Welcome to</p>
