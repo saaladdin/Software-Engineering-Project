fb89517131539bf71e656bb6c89bc0f930a323e5
Hamzah <muhammadh3@montclair.edu>
2025-04-29 01:40:35 -0400

    made some changes to event details page

--- src/Components/EventDetails/EventDetails.js
+++ src/Components/EventDetails/EventDetails.js
@@ -4,48 +4,61 @@ import "./EventDetails.scss";
 
 const EventDetails = () => {
   const { state } = useLocation();
-  
-  const { title, time, location, image, description, organization, tags } = state.event;
+  const { title, time, location, image, description, organization, tags } =
+    state.event;
 
- return (
-   <div className="eventDetails">
-     <div className="eventImageContainer">
-       <img src={image} alt={title} className="event-image" />
-     </div>
+  const tagDisplayMap = {
+    "free-food": "Free food",
+    "free-stuff": "Free stuff",
+  };
 
-     <div className="eventDetailsText">
-       <h3 className="event-title">{title}</h3>
-       <p className="event-time">
-         <strong>Date:</strong> {time}
-       </p>
-       <p className="event-location">
-         <strong>Location:</strong> {location}
-       </p>
-       <p className="event-organization">
-         <strong>Organization:</strong> {organization}
-       </p>
-       <button className="register-button">Register for this event</button>
+  return (
+    <div className="eventDetails">
+      <div className="eventMainSection">
+        <div className="eventImageContainer">
+          <img src={image} alt={title} className="eventImage" />
+        </div>
 
-       {tags && tags.length > 0 && (
-         <div className="tags-container">
-           {tags.map((tag, index) => (
-             <span key={index} className="event-tag">
-               {tag}
-             </span>
-           ))}
-         </div>
-       )}
-     </div>
+        <div className="eventDetailsContent">
+          <h1 className="eventTitle">{title}</h1>
+          <p className="eventInfo">
+            <strong>Date:</strong> {time} <br />
+            <strong>Location:</strong> {location}
+            <br />
+            <strong>Organization:</strong> {organization}
+          </p>
+          <button className="registerButton">Register for this event</button>
 
-     {/* Move the description box BELOW the image and details */}
-     {description && (
-       <div className="description-box">
-         <p className="event-description">{description}</p>
-       </div>
-     )}
-   </div>
- );
+          {tags && tags.length > 0 && (
+            <div className="eventTags">
+              <p className="tagsLabel">Tags:</p>
+              {tags.map((tag, index) => (
+                <span key={index} className="eventTag">
+                  {tagDisplayMap[tag] || tag}
+                </span>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
 
+      {description && (
+        <div className="descriptionBox">
+          <ul>
+            {description
+              .split("\n")
+              .map((line, index) =>
+                line.startsWith("-") ? (
+                  <li key={index}>{line.slice(1).trim()}</li>
+                ) : (
+                  <p key={index}>{line.trim()}</p>
+                )
+              )}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default EventDetails;
